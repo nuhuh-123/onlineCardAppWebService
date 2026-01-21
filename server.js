9320243a56f90e029807dc8123cef67a04d231e4
zy <24047368@myrp.edu.sg>
2026-01-21 10:41:32 +0800

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
 require('dotenv').config();
-const port = 3000;
+const port = process.env.PORT || 3000;
  
 //database config info
 const dbConfig = {
